bb0d185ae5356f1bde220bf9c238d893360c2d77
jakobrs <jakobrs100@gmail.com>
2023-01-10 09:53:58 +0100

    Lynnedslag: Add complete Python impl
    (it's still way too slow)

--- lynnedslag/lynnedslag.py
+++ lynnedslag/lynnedslag.py
@@ -1,11 +1,48 @@
+from typing import List
+
+
+class SegmentTree:
+    offset: int
+    values: List[bool]
+
+    def __init__(self, size: int) -> None:
+        self.values = [False] * (2 * size)
+        self.offset = size
+
+    def update(self, left: int, right: int) -> None:
+        def update_internal(v: int, start: int, end: int) -> None:
+            if left <= start and end <= right:
+                self.values[v] = not self.values[v]
+            elif end <= left or right <= start:
+                pass
+            else:
+                mid = (start + end) // 2
+                update_internal(2 * v, start, mid)
+                update_internal(2 * v + 1, mid, end)
+
+        update_internal(1, 0, self.offset)
+
+    def count(self) -> int:
+        def count_internal(v: int, invert: bool) -> int:
+            here = invert ^ self.values[v]
+            if v >= self.offset:
+                return here
+            else:
+                return count_internal(2 * v, here) + count_internal(2 * v + 1, here)
+
+        return count_internal(1, False)
+
+
 n, m = map(int, input().split())
 
-a = [True] * n
+rounded_n = 1
+while rounded_n < n:
+    rounded_n *= 2
+
+st = SegmentTree(rounded_n)
 
 for _ in range(m):
     l, r = map(int, input().split())
-    a[l:r+1] = [not x for x in a[l:r+1]]
-    # Note: This is much slower for subtask 2
-    # a[:] = a[:l] + [not x for x in a[l:r+1]] + a[r+1:]
+    st.update(l, r + 1)
 
-print(sum(a))
+print(n - st.count())
